fce459ff975a74266f3fbcc3d0850f33d7b09202
Terence Honles <terence@honles.com>
2025-07-29 13:02:01 +0200

    add type information and general clean-up (#52)
    This change adds type information, performs general clean-up, and
    properly escapes the HTML output.

--- sphinxcontrib_images_lightbox2/lightbox2.py
+++ sphinxcontrib_images_lightbox2/lightbox2.py
@@ -1,6 +1,15 @@
+import html
 
-import os
 from sphinxcontrib import images
+
+
+def _a(value: str) -> str:
+    return html.escape(value, quote=True)
+
+
+def _h(value: str) -> str:
+    return html.escape(value)
+
 
 class LightBox2(images.Backend):
     STATIC_FILES = (
@@ -15,55 +24,56 @@ class LightBox2(images.Backend):
         'lightbox2-customize/pointer.css'
     )
 
-    def visit_image_node_html(self, writer, node):
+    def visit_image_node_html(
+        self, writer: images.Writer, node: images.image_node
+    ) -> None:
         # make links local (for local images only)
         builder = self._app.builder
         if node['uri'] in builder.images:
-            node['uri'] = '/'.join([builder.imgpath,
-                                    builder.images[node['uri']]])
+            node['uri'] = '/'.join([builder.imgpath, builder.images[node['uri']]])
 
-        if node['show_caption'] is True:
-            writer.body.append(
-            u'''<figure class="{cls}">
-            '''.format(cls=' '.join(node['classes']),))
+        # add wrapping optional figure tag and then anchor tag
+        if node['show_caption']:
+            writer.body.append(f'<figure class="{_a(" ".join(node["classes"]))}">')
+            writer.body.append('<a ')
             if node['legacy_classes']:
-                writer.body.append(
-                u'''<a class="{legcls}"'''.format(legcls=' '.join(node['legacy_classes']),))
-            else:
-                writer.body.append(u'''<a ''')
+                writer.body.append(f'class="{_a(" ".join(node["legacy_classes"]))}" ')
         else:
-            writer.body.append(u'''<a class="{cls}"'''.format(cls=' '.join(node['classes']),))
+            writer.body.append(f'<a class="{_a(" ".join(node["classes"]))}" ')
 
-        writer.body.append(
-            u'''
-               data-lightbox="{group}"
-               href="{href}"
-               title="{title}"
-               data-title="{title}"
-               {id}
-               >'''.format( group='group-%s' % node['group'] if node['group'] else node['uri'],
-                            href=node['uri'],
-                            title=node['title'] + node['content'],
-                            # Only one id attribute is meaningful
-                            id = 'id="' + node['ids'][0] + '"' if len(node['ids'])>0 else ''
-                            ))
-        writer.body.append(
-            '''<img src="{src}"
-                     class="{cls}"
-                     width="{width}"
-                     height="{height}"
-                     alt="{alt}"/>
-                '''.format(src=node['uri'],
-                           cls='align-%s' % node['align'] if node['align'] else '',
-                           width=node['size'][0],
-                           height=node['size'][1],
-                           alt=node['alt'],
-                           title=node['title']))
+        # add anchor attributes
+        link_title = f'{node["title"]}{node["content"]}'
+        for attr, value in (
+            ('data-lightbox', f'group-{node["group"]}' if node['group'] else node['uri']),
+            ('href', node['uri']),
+            ('title', link_title),
+            ('data-title', link_title),
+            # Only one id attribute is meaningful
+            *([('id', node['ids'][0])] if node['ids'] else ()),
+        ):
+            writer.body.append(f'{attr}="{_a(value)}" ')
+
+        # finish anchor tag and start image tag
+        writer.body.append(f'><img ')
+
+        # add image attributes
+        for attr, value in (
+            ('src', node["uri"]),
+            ('width', node['size'][0]),
+            ('height', node['size'][1]),
+            ('alt', node['alt']),
+            ('title', node['title']),
+            *([('class', f'align-{node["align"]}')] if node['align'] else ()),
+        ):
+            writer.body.append(f'{attr}="{_a(value)}" ')
+
+        writer.body.append(f'/>')
 
 
-    def depart_image_node_html(self, writer, node):
+    def depart_image_node_html(
+        self, writer: images.Writer, node: images.image_node
+    ) -> None:
         writer.body.append('</a>')
-        if node['show_caption'] is True:
-            writer.body.append(u'''<figcaption>{title}</figcaption>
-            '''.format(title=node['title'],))
+        if node['show_caption']:
+            writer.body.append(f'<figcaption>{_h(node["title"])}</figcaption>')
             writer.body.append('</figure>')
